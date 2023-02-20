30b5292f6d572daf1f5dbea1e7a9cdaaded8a95c
Zach Lipp <zlipp@protonmail.com>
2023-02-20 10:04:26 -0800

    trace hub service

--- example-services/hub/main.py
+++ example-services/hub/main.py
@@ -3,7 +3,25 @@ from typing import Literal
 
 import requests
 from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from pydantic import BaseModel
+
+resource = Resource(attributes={"service.name": "hub"})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 
 class NumberContainer(BaseModel):
@@ -27,6 +45,7 @@ def call_remote_service(
 
 
 app = FastAPI()
+FastAPIInstrumentor.instrument_app(app)
 
 
 @app.post("/")
@@ -35,7 +54,7 @@ def fizzbuzz(nc: NumberContainer):
     fizz = call_remote_service(number, "fizzer")
     buzz = call_remote_service(number, "buzzer")
     if fizz and buzz:
-        return  f"{number}: Fizzbuzz!"
+        return f"{number}: Fizzbuzz!"
     elif fizz:
         return f"{number}: Fizz!"
     elif buzz:
