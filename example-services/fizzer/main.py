61c3245e319f827f45ebe57c61a2dccfa77af27d
Zach Lipp <zlipp@protonmail.com>
2023-02-20 12:24:35 -0800

    propogate headers and trace in fizzer

--- example-services/fizzer/main.py
+++ example-services/fizzer/main.py
@@ -1,16 +1,48 @@
+import json
 import time
 
-from flask import Flask, jsonify, request
+from flask import Flask, make_response, request
+from opentelemetry import trace
+from opentelemetry.context import Context
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.propagate import inject
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace.propagation import tracecontext
+
+FORMAT = tracecontext.TraceContextTextMapPropagator()
+
+resource = Resource(attributes={"service.name": "fizzer"})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
 def fizz():
-    time.sleep(.5)
-    x = request.json["number"]
-    fizz = bool(x % 3 == 0)
-    return jsonify({"result": fizz})
+    traceparent = request.headers.get("traceparent")
+    with tracer.start_as_current_span(
+        "/", context=FORMAT.extract({"traceparent": traceparent})
+    ) as fizzspan:
+        headers = {}
+        inject(headers)
+        user_agent = request.headers.get("user-agent")
+        fizzspan.set_attribute("http.user_agent", user_agent)
+        time.sleep(0.5)
+        x = request.json["number"]
+        fizz = bool(x % 3 == 0)
+        return make_response(json.dumps({"result": fizz}), 200, headers)
 
 
 if __name__ == "__main__":
